5948c292198a3e7b6a41d46c512806fe74e13c78
Dairo Facundo <47672079+dairof7@users.noreply.github.com>
2020-08-12 16:18:09 -0500

    warning 1

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from models.place import Place
-from sqlalchemy import Column, String, Integer
-from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
-    """ Class Amenity """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary='place_amenity')
+    name = ""
