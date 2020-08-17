cce2e5aca23c88115c7fca2832bff5e3b8f7845c
somarae8 <somarae8@gmail.com>
2020-08-17 19:33:28 +0000

    fix 0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo echo "<html>
+echo "<html>
   <head>
   </head>
   <body>
