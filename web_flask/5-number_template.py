303800279b7c73019baccd4db1b543d440c12f9e
somarae8 <somarae8@gmail.com>
2020-09-01 23:34:11 +0000

    5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -44,7 +44,7 @@ def message_5(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def message_6(n):
     """return web page if n is a number"""
-    return render_template('5-number.html', n=number)
+    return render_template('5-number.html', number=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
