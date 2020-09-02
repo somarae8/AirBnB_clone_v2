91a90858437fe1e79b3f88a25b4571f7f8ad534b
somarae8 <somarae8@gmail.com>
2020-09-02 16:28:32 +0000

    update

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Start a Flask web application"""
+
+
+from models import storage
+from flask import Flask, render_template
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_db(self):
+    """Close session"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def stateList():
+    """function to execute"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cityList():
+    """function to execute"""
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
